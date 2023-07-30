1950a6fe116c9928c1a7ac04139b7d338f64fa8a
Joseph Youngquist <joseph.youngquist@gmail.com>
2023-07-30 14:45:30 -0400

    adds Player movement from user input sets Unity editor for Player poistion to 0, 0, 0

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 5.5f; // initial player speed
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float inputDirection = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * inputDirection * _speed * Time.deltaTime);
     }
 }
