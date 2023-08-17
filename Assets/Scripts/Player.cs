25f51e6b113675b47bc9ee9bbfbd267c43c5c130
Joseph Youngquist <joseph.youngquist@gmail.com>
2023-08-17 01:32:00 -0400

    clean up some code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,5 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
